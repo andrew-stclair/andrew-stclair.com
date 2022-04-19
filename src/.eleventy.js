d5f5ab6c8892032b5738287581d22ead37c92ff6
Andrew St Clair <andrew_stclair95@hotmail.com>
2022-04-19 21:11:33 +1000

    Passthrough Copy favicon.ico
    Signed-off-by: Andrew St Clair <andrew_stclair95@hotmail.com>

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -2,4 +2,5 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/.well-known");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
 };

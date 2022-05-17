a6c68226fa43f6192c53438610c915f95a7d26da
Andrew St Clair <andrew_stclair95@hotmail.com>
2022-05-17 22:17:08 +1000

    Initial blog work
    Signed-off-by: Andrew St Clair <andrew_stclair95@hotmail.com>

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,6 +1,10 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/.well-known");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };

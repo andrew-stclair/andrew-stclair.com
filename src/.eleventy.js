d019dd45cc00cc6bfc68af675162b8e2b05ca03e
Andrew St Clair <andrew_stclair95@hotmail.com>
2022-05-18 22:30:01 +1000

    Finish Blog
    Signed-off-by: Andrew St Clair <andrew_stclair95@hotmail.com>

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
@@ -9,4 +11,25 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(pluginRss);
+
+    eleventyConfig.addFilter("readableDateTime", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'Australia/Melbourne' }).toLocaleString(DateTime.DATETIME_FULL);
+    });
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('urlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'Australia/Melbourne' }).toFormat('yyyy/LL/dd');
+    });
+
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (!Array.isArray(array) || array.length === 0) {
+            return [];
+        }
+        if (n < 0) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
+    });
 };
